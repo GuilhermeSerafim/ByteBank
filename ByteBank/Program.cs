451810c899becd664956cbf89b455ff3eb0ae630
GuilhermeSerafim <guiserafim2017@gmail.com>
2024-10-09 14:29:16 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 20;
+    idades[1] = 30;
+    idades[2] = 40;
+    idades[3] = 50;
+    idades[4] = 60;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    int totaldeNotas = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Índice [{i}] = {idade}");
+        totaldeNotas += idade;
+    }
+
+    int media = totaldeNotas / idades.Length;
+    Console.WriteLine("Média: " + media);
+}
