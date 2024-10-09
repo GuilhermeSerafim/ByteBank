223e1a8b9afada4770ceb57ada06c229ee1b0055
GuilhermeSerafim <guiserafim2017@gmail.com>
2024-10-09 14:41:09 -0300

    Aprendendo a usar o for com array e o foreach

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,4 +1,6 @@
-﻿void TestaArrayInt()
+﻿Console.WriteLine("Bem vindo ao Byte Bank");
+TestaBuscarPalavra();
+void TestaArrayInt()
 {
     int[] idades = new int[5];
     idades[0] = 20;
@@ -19,4 +21,29 @@
 
     int media = totaldeNotas / idades.Length;
     Console.WriteLine("Média: " + media);
+}
+
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.WriteLine($"Digite uma palavra");
+        arrayDePalavras[i] = Console.ReadLine()!;
+
+    }
+
+    Console.WriteLine("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+    foreach (string s in arrayDePalavras)
+    {
+        if (s.Equals(busca))
+        {
+            Console.WriteLine($"A palavra é {s}");
+            break;
+        } else
+        {
+            Console.WriteLine("Palavra não encontrada");
+        }
+    }
 }
